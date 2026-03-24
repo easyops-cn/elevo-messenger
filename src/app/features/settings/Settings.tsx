6846d8512fb126a159717be50e73cefc72f2bd0a
Shenwei Wang <wangshenwei@qq.com>
2026-03-24 12:30:37 +0800

    fix: disable connectors settings

--- src/app/features/settings/Settings.tsx
+++ src/app/features/settings/Settings.tsx
@@ -30,7 +30,7 @@ import { Notifications } from './notifications';
 import { Devices } from './devices';
 import { EmojisStickers } from './emojis-stickers';
 import { DeveloperTools } from './developer-tools';
-import { Connectors } from './connectors';
+// import { Connectors } from './connectors';
 import { About } from './about';
 import { UseStateProvider } from '../../components/UseStateProvider';
 import { stopPropagation } from '../../utils/keyboard';
@@ -43,7 +43,7 @@ export enum SettingsPages {
   DevicesPage,
   EmojisStickersPage,
   DeveloperToolsPage,
-  ConnectorsPage,
+  // ConnectorsPage,
   AboutPage,
 }
 
@@ -86,11 +86,11 @@ const useSettingsMenuItems = (): SettingsMenuItem[] =>
         nameKey: 'settings.developerTools',
         icon: Icons.Terminal,
       },
-      {
-        page: SettingsPages.ConnectorsPage,
-        nameKey: 'settings.connectors',
-        icon: Icons.Link,
-      },
+      // {
+      //   page: SettingsPages.ConnectorsPage,
+      //   nameKey: 'settings.connectors',
+      //   icon: Icons.Link,
+      // },
       {
         page: SettingsPages.AboutPage,
         nameKey: 'settings.about',
@@ -237,9 +237,9 @@ export function Settings({ initialPage, requestClose }: SettingsProps) {
       {activePage === SettingsPages.DeveloperToolsPage && (
         <DeveloperTools requestClose={handlePageRequestClose} />
       )}
-      {activePage === SettingsPages.ConnectorsPage && (
+      {/* {activePage === SettingsPages.ConnectorsPage && (
         <Connectors requestClose={handlePageRequestClose} />
-      )}
+      )} */}
       {activePage === SettingsPages.AboutPage && <About requestClose={handlePageRequestClose} />}
     </PageRoot>
   );
