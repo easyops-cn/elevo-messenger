04db290bb1d1277dec75cffc838f8675569b8c71
Shenwei Wang <wangshenwei@qq.com>
2026-05-28 19:56:17 +0800

    fix: refine paste image name

--- src/app/hooks/useFilePasteHandler.ts
+++ src/app/hooks/useFilePasteHandler.ts
@@ -1,10 +1,18 @@
+import dayjs from 'dayjs';
 import { useCallback, ClipboardEventHandler } from 'react';
-import { getDataTransferFiles } from '../utils/dom';
+import { getDataTransferFiles, renameFile } from '../utils/dom';
+
+const getClipboardImageName = (): string => `image-${dayjs().format('YYMMDD-HHmmss')}.png`;
+
+const renameDefaultClipboardImage = (file: File): File => {
+  if (file.name !== 'image.png' || file.type !== 'image/png') return file;
+  return renameFile(file, getClipboardImageName());
+};
 
 export const useFilePasteHandler = (onPaste: (file: File[]) => void): ClipboardEventHandler =>
   useCallback(
     (evt) => {
-      const files = getDataTransferFiles(evt.clipboardData);
+      const files = getDataTransferFiles(evt.clipboardData)?.map(renameDefaultClipboardImage);
       if (files) onPaste(files);
     },
     [onPaste]
