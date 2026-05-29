2ea58e5c1728f69c28a9fb1d93fb56f0839fff16
Shenwei Wang <wangshenwei@qq.com>
2026-05-29 11:40:54 +0800

    chore: upgrade prettier and eslint to the latest

---
+++ eslint.config.js
@@ -0,0 +1,95 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { fixupConfigRules } from '@eslint/compat';
+import globals from 'globals';
+import prettierConfig from 'eslint-config-prettier';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import tsParser from '@typescript-eslint/parser';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['dist/**', 'experiment/**', 'node_modules/**'],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'airbnb',
+    ),
+  ),
+  prettierConfig,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        JSX: 'readonly',
+        __APP_VERSION__: 'readonly',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'linebreak-style': 0,
+      'no-underscore-dangle': 0,
+      'no-shadow': 'off',
+      'no-unused-vars': 'off',
+      'no-nested-ternary': 'off',
+      'default-case': 'off',
+      'consistent-return': 'off',
+
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+        },
+      ],
+
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.tsx', '.jsx'],
+        },
+      ],
+
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-shadow': 'error',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+];
